045a06f1e5e33a6154a09ea7478dcd57cca375ab
Artyom_Kazagashev <tema124ru@mail.ru>
2024-09-23 22:23:16 +0700

    fixes category

--- src/category.py
+++ src/category.py
@@ -29,7 +29,6 @@ class Category:
             product_str = f"{product.name}, {product.price}руб. Остаток: {product.quantity}шт.\n"
         return product_str
 
-
     def add_product(self, product):
         """Добавляет товар в категорию"""
         self.__products.append(product)
