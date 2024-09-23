2be53b165b0a41f6e10247f9c63d18c6be37a3b6
Artyom_Kazagashev <tema124ru@mail.ru>
2024-09-23 16:46:53 +0700

    add __product

--- src/product.py
+++ src/product.py
@@ -9,5 +9,43 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, param_product):
+        return cls(**param_product)
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевой или отрицательной")
+        else:
+            self.__price = new_price
+
+
+
+if __name__ == "__main__":
+    new_product = Product.new_product(
+        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0, "quantity": 5})
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
+
+
+
+
+
