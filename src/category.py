a6b283c80d6767aeb7926151c9705925c2bf71bb
Artyom_Kazagashev <tema124ru@mail.ru>
2024-09-23 20:48:54 +0700

    add category and test

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс, представляющий категорию продуктов"""
 
@@ -10,6 +13,35 @@ class Category:
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += len(self.__products)
+
+
+    @property
+    def products(self):
+        return self.__products
+
+
+    @property
+    def products_list(self):
+        product_str = ""
+        for product in self.__products:
+            product_str = f"{product.name}, {product.price}. Остаток: {product.quantity}\n"
+        return product_str
+
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+
+
+
+
+
+
+
+
